10a2b9347307f195d8bac720e1ecfcec31dacdcc
ImtiazSikder <sikderimtiaz@yahoo.com>
2022-12-02 12:25:42 -0500

    Commit Login Test

---
+++ src/test/java/pkg/LoginTest.java
@@ -0,0 +1,11 @@
+package pkg;
+
+public class LoginTest {
+
+    public static void main(String[] args){
+        System.out.println("Login with Valid Username");
+        System.out.println("Login with Valid Password");
+        System.out.println("Login with Invalid Password");
+
+    }
+}
